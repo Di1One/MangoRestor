4f750290970ff8a4497af2e5a450f965918dd22b
Kirill Vdovenko <60675403+Di1One@users.noreply.github.com>
2023-04-04 21:30:07 +0300

    Cart Repository Interface for Shopping Cart API

---
+++ Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
@@ -0,0 +1,27 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Mango.Services.ShoppingCartAPI.Repository
+{
+    public class CartRepository : ICartRepository
+    {
+        public async Task<bool> ClearCartAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CartDto> CreateUpdateCartAsync(CartDto cartDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CartDto> GetCartByUserIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> RemoveFromCartAsync(int cartDetaitlsId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
